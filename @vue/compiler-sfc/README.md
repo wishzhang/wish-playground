05144299a23d224de89ce926cc8f853efbc21781
wishzhang <wishzhang.io@qq.com>
2022-12-15 22:00:59 +0800

    feat

---
+++ @vue/compiler-sfc/README.md
@@ -0,0 +1,3 @@
+https://www.npmjs.com/package/@vue/compiler-sfc
+
+source: https://github.com/vuejs/core/tree/main/packages/compiler-sfc
--- @vue/compiler-sfc/index.ts
+++ @vue/compiler-sfc/index.ts
@@ -1,3 +1,42 @@
-let a:number = 3;
+import {compileTemplate, parse} from '@vue/compiler-sfc'
 
-console.log('start...: ' + a)
+// no parseComponent API
+
+/*const res = compileTemplate(
+  {
+    id: 'test',
+    filename: 'example.vue',
+    source: `
+      <template>
+        <div>hi</div>
+      </template>
+    `
+  }
+)*/
+
+const res = parse(
+  `
+      <template>
+        <div>hi</div>
+      </template>
+      <style src="./test.css"></style>
+      <style lang="stylus" scoped>
+        h1
+          color red
+        h2
+          color green
+      </style>
+      <style module>
+        h1 { font-weight: bold }
+      </style>
+      <style bool-attr val-attr="test"></style>
+      <script>
+        export default {}
+      </script>
+      <div>
+        <style>nested should be ignored</style>
+      </div>
+    `
+)
+
+console.log(res)

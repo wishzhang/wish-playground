38e641d4538f3a249e5408244efec3af72f0f55b
wishzhang <wishzhang.io@qq.com>
2023-03-03 23:50:08 +0800

    wip: drag of visual

---
+++ visual/drag/src/components/Canvaser.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="canvas-bg" @drop="handleDrop" @dragover="handleDragover">canss</div>
+</template>
+
+<script>
+  export default {
+    name: "Canvas",
+    methods: {
+      handleDrop(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log(e.dataTransfer.getData('id'))
+      },
+      handleDragover(e) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = "copy"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .canvas-bg {
+    background: white;
+  }
+</style>
---
+++ visual/drag/src/components/Material.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="material">
+    <div v-for="(item, index) in materials" :id="item.id" :key="index" :style="{background: item.color}"
+         @dragstart="handleDragstart"
+         :draggable="true"
+         class="material-item"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Material",
+    data() {
+      return {
+        materials: [
+          {
+            id: 'a1',
+            color: '#123456'
+          },
+          {
+            id: 'a2',
+            color: '#654321'
+          },
+          {
+            id: 'a3',
+            color: '#351246'
+          }
+        ]
+      }
+    },
+    methods: {
+      handleDragstart(e) {
+        e.dataTransfer.dropEffect = "copy";
+        e.dataTransfer.setData('id', e.target.id)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .material {
+    .material-item {
+      height: 80px;
+    }
+  }
+</style>

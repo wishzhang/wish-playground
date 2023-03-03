edb1cc6d241ed83a2e75e35cd8c8f91f584e6042
wishzhang <wishzhang.io@qq.com>
2023-03-03 18:06:11 +0800

    wip: update

---
+++ visual/src/components/Designer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="designer">
+    <div class="material">
+      <Material/>
+    </div>
+    <div class="canvaser">
+      <Canvaser/>
+    </div>
+    <div class="setting">
+      <Setting/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Material from '@/components/Material';
+  import Setting from '@/components/Setting';
+  import Canvaser from '@/components/Canvaser';
+  export default {
+    name: "Designer",
+    components: {
+      Material,
+      Setting,
+      Canvaser
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .designer {
+    display: flex;
+    height: 100%;
+
+    .material {
+      width: 200px;
+      background: grey;
+    }
+
+    .canvaser {
+      flex: 1;
+      background: #1a1a1a;
+    }
+
+    .setting {
+      width: 200px;
+      background: #2c3e50;
+    }
+  }
+</style>
---
+++ visual/src/components/Material.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="material">
+    <div v-for="(item, index) in colors" :key="index" :style="{background: getColor(index)}" class="material-item"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Material",
+    data() {
+      return {
+        colors: ['#123456', '#654321', '#351246']
+      }
+    },
+    methods: {
+      getColor(i) {
+        return this.colors[i]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .material {
+    .material-item {
+      height: 80px;
+    }
+  }
+</style>

74768409822d19ac067cc253a5e017e75c7feec6
wishzhang <wishzhang.io@qq.com>
2023-03-05 16:15:15 +0800

    wip: update

---
+++ visual/echarts/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Adaptive></Adaptive>
+  </div>
+</template>
+
+<script>
+import Adaptive from '@/components/Adaptive';
+
+export default {
+  name: 'App',
+  components: {
+    Adaptive
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ visual/vue-grid-layout-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Basic/>
+  </div>
+</template>
+
+<script>
+import Basic from './components/Basic.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Basic
+  }
+}
+</script>
+
+<style>
+</style>

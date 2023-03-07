d8d57a8dea92dc88bc9dbf88f79888b5f79749d7
wishzhang <wishzhang.io@qq.com>
2023-03-07 18:22:06 +0800

    wip: update

---
+++ visual/transform/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <div class="outer-box2">
+      <div class="outer-box">
+        <div class="scale-box">
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </div>
+      </div>
+    </div>
+    <button @click="handleClick">改变大小</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    handleClick() {
+      let el = document.querySelector('.scale-box')
+      console.log(el)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .outer-box2 {
+    width: 300px;
+    overflow: auto;
+    .outer-box {
+      width: 200px;
+      height: auto;
+      background: gray;
+      //overflow: auto;
+
+      .scale-box {
+        width: 400px;
+        background: lightblue;
+        //transform: scale(0.2);
+        //transform-origin: 0 0;
+      }
+    }
+  }
+</style>
